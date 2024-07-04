3aabba95f46cb759ca72768f4a0b308b144a9ab3
Sarita <131792757+sarita1509@users.noreply.github.com>
2024-07-04 08:37:16 +0000

    "commit"

---
+++ level2/grid/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GridComponent from './GridComponent';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <GridComponent />
+    </div>
+  );
+}
+
+export default App;
